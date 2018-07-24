4c85d22d9b253ec13b10a168b0248e4c1b271df5
Maria <lilllit888@gmail.com>
2018-07-24 07:17:00 +0600

    решение 11

--- IdeaProjects/firsttest/src/test/java/ru/broker/BaseTest.java
+++ IdeaProjects/firsttest/src/test/java/ru/broker/BaseTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.security.SecureRandom;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -17,6 +18,7 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 public class BaseTest {
     public static WebDriver driver;
     public static WebDriverWait wait;
+
 
     @Before
     public void setup() {
@@ -31,6 +33,15 @@ public class BaseTest {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+    }
+
+    public static String getRandomString (int l,String basestring){
+        StringBuilder b=new StringBuilder();
+        for (int i=0;i<l;l++){ b.append(basestring.charAt((int)(Math.random()*basestring.length())));
+            //b.append(c);
+
+        }
+        return b.toString();
     }
 
 
---
+++ IdeaProjects/firsttest/src/test/java/ru/broker/EleventhTest.java
@@ -0,0 +1,95 @@
+package ru.broker;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.UUID;
+
+import static junit.framework.TestCase.fail;
+
+public class EleventhTest extends BaseTest  {
+
+    @Test
+    public void userAuth() {
+        java.lang.String ss1=new java.lang.String();
+        java.lang.String ss2=new java.lang.String();
+        int r=Math.max((int)(Math.random()*10),5);
+        ss1= UUID.randomUUID().toString().replace("-","").substring(0,r);
+        ss2= UUID.randomUUID().toString().replace("-","").substring(0,5);
+        java.lang.String email = ss1+"@" + ss2 + ".com";
+        //2й вариант получения рандомной строки
+        //ss1=Long.toHexString(Double.doubleToLongBits(Math.random())).substring(0,r);
+        //3й вариант рандомной строки - описание класса RandomString скачано с qaru.site
+        // RandomString ss=new RandomString(5);
+        //java.lang.String email = "" + ss + ".com";
+
+        driver.get("http://localhost/litecart/en/");
+
+        java.lang.String firstName = "Кофе";
+        java.lang.String lastName = "Крепкий";
+        java.lang.String adress = "adress";
+        java.lang.String Code = "123456";
+        java.lang.String city = "Новосибирск";
+        java.lang.String country = "Russian Federation";
+        java.lang.String phone = "9139297451";
+        String password = "password";
+
+
+        driver.navigate().to( "http://localhost/litecart" );
+
+        WebElement w1 = driver.findElement( By.xpath( ".//a[text() = 'Create Account']" ) );
+        w1.click();
+
+        WebElement wfirstName = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'firstname')]"));
+        wfirstName.sendKeys(firstName);
+
+        WebElement wlastName = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'lastname')]"));
+        wlastName.sendKeys(lastName);
+
+        WebElement wadress = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'address1')]"));
+        wadress.sendKeys(adress);
+
+        WebElement wpostcode = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'postcode')]"));
+        wpostcode.sendKeys(Code);
+
+        WebElement wcity = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'city')]"));
+        wcity.sendKeys(city);
+
+        WebElement wcntrCode = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//select[contains(@name,'country_code')]"));
+        new Select( wcntrCode ).selectByVisibleText(java.lang.String.valueOf(country));
+
+        WebElement wemail = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'email')]"));
+        wemail.sendKeys( email );
+
+        WebElement wphone = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'phone')]"));
+        wphone.sendKeys(phone);
+
+        WebElement wpswd = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'password')]"));
+        wpswd.sendKeys( password );
+
+        WebElement wconfirm = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//input[contains(@name,'confirmed_password')]"));
+        wconfirm.sendKeys(password);
+
+        WebElement button = driver.findElement(By.xpath("//div[contains(@id,'create-account')]//button[contains(@name,'create_account')]"));
+        button.click();
+
+        driver.findElement( By.xpath( "//a[contains(text(),'Logout')]" ) ).click();
+
+        /*try {
+             Thread.sleep(20000);
+             } catch (InterruptedException e) {
+               e.printStackTrace();
+             }*/
+
+        driver.findElement( By.name( "email" ) ).sendKeys(email);
+        driver.findElement( By.name( "password" ) ).sendKeys(password);
+        driver.findElement( By.name( "login" ) ).click();
+
+        driver.findElement( By.xpath("//a[contains(text(),'Logout')]")).click();
+
+        }
+}
+
